8501f377ffc35bf828c86e0560b3bedee3ad5911
Eric Rochester <erochest@virginia.edu>
2014-12-17 07:14:25 -0500

    Other dependencies and stub out report types.

--- Main.hs
+++ Main.hs
@@ -1,18 +1,61 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 
 module Main where
 
 
-import qualified Data.Text           as T
-import           Options.Applicative
+import           Control.Lens
+import qualified Data.Text                   as T
+-- import qualified Data.Text.Lazy              as TL
+-- import           Data.Text.Lazy.Builder
+-- import           Network.Wreq
+import           Options.Applicative         hiding (header)
+import qualified Options.Applicative         as O
+import           Text.Blaze.Html5            hiding (header, option)
+-- import qualified Text.Blaze.Html5            as H
+-- import qualified Text.Blaze.Html5.Attributes as A
 
 
 data GhWeekly
         = GhWeekly
-        { ghwUser :: !T.Text
-        , ghwOrgs :: !(Maybe T.Text)
+        { _ghwUser :: !T.Text
+        , _ghwOrgs :: !(Maybe T.Text)
         } deriving (Show)
+makeLenses ''GhWeekly
+
+data User
+data Repo
+data Event
+data Commit
+
+data RepoReport
+        = RepoReport
+        { _rrRepo        :: !Repo
+        , _rrIssueEvents :: ![Event]
+        , _rrCommits     :: ![Commit]
+        }
+makeLenses ''RepoReport
+
+data UserReport
+        = UserReport
+        { _reportUser :: !User
+        , _reportRepo :: !RepoReport
+        }
+makeLenses ''UserReport
+
+
+renderUser :: UserReport -> Html
+renderUser = undefined
+
+renderRepo :: RepoReport -> Html
+renderRepo = undefined
+
+renderIssueEvent :: Event -> Html
+renderIssueEvent = undefined
+
+renderCommit :: Commit -> Html
+renderCommit = undefined
 
 
 main :: IO ()
@@ -33,7 +76,7 @@ opt :: ParserInfo GhWeekly
 opt = info (helper <*> opt')
            (  fullDesc
            <> progDesc "Produce a report of a user's Github activity for a week."
-           <> header "gh-weekly -- report a user's Github activity.")
+           <> O.header "gh-weekly -- report a user's Github activity.")
 
 textReader :: ReadM T.Text
 textReader = T.pack <$> str
