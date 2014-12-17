253c4c8a1653e8337828376c55e3bb80d1226da7
Eric Rochester <erochest@virginia.edu>
2014-12-16 22:15:55 -0500

    Stub out command-line options.

--- Main.hs
+++ Main.hs
@@ -4,5 +4,36 @@
 module Main where
 
 
+import qualified Data.Text           as T
+import           Options.Applicative
+
+
+data GhWeekly
+        = GhWeekly
+        { ghwUser :: !T.Text
+        , ghwOrgs :: !(Maybe T.Text)
+        } deriving (Show)
+
+
 main :: IO ()
-main = undefined
+main = print =<< execParser opt
+
+
+opt' :: Parser GhWeekly
+opt' =   GhWeekly
+     <$> option textReader
+                (  short 'u' <> long "user" <> metavar "USERNAME"
+                <> help "The user to report on.")
+     <*> (optional . option textReader
+                   $  short 'o' <> long "org" <> metavar "ORGNAME"
+                   <> help "An optional organization to look for\
+                           \ commits in also.")
+
+opt :: ParserInfo GhWeekly
+opt = info (helper <*> opt')
+           (  fullDesc
+           <> progDesc "Produce a report of a user's Github activity for a week."
+           <> header "gh-weekly -- report a user's Github activity.")
+
+textReader :: ReadM T.Text
+textReader = T.pack <$> str
